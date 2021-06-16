222313798340a155305b46513d852559a1cab028
Mario Rodriguez <mariordgez@hotmail.com>
2021-06-16 12:36:10 -0500

    Add before action to controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: %i[ show edit update destroy ]
-  http_basic_authenticate_with :name => "dhh", :password => "secret", :except => [:index, :show]
+  before_action :authenticate_user!, only: [ :new, :create ]
 
   # GET /posts or /posts.json
   def index
