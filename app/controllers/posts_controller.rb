965fd02e9e19d0eda904161792d435d065dccab5
Mario Rodriguez <mariordgez@hotmail.com>
2021-06-17 04:01:28 -0500

    Fix destroy method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -49,11 +49,10 @@ class PostsController < ApplicationController
 
   # DELETE /posts/1 or /posts/1.json
   def destroy
+    @post = Post.find(params[:id])
     @post.destroy
-    respond_to do |format|
-      format.html { redirect_to posts_url, notice: "Post was successfully destroyed." }
-      format.json { head :no_content }
-    end
+
+    redirect_to root_path
   end
 
   private
