6c095bdfd27c7c92a90d5b51f61340368fed9f0a
angelacuahutle <angela.cuahutle@gmail.com>
2021-06-17 15:56:12 -0500

    Fix mild errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def signed_in_only!
+    #current_user in ApplicationController must be defined as helper method to make it work
+    redirect_to new_user_session_path unless current_user
+   
+    # alternative solution:
+    # if signed_in? is defined in ApplicationController 
+    # redirect_to new_user_session_path unless signed_in?
+   end
 end
